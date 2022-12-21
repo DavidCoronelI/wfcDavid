8fb179dd12cc146e3c93aaa45881ab7ec94dd442
Cyro <david.coroneli@ucuenca.edu.ec>
2022-12-21 14:07:43 -0500

    ingreso y estructura de datos

--- sketch.js
+++ sketch.js
@@ -1,7 +1,129 @@
+const celd = [];
+const RCTC = 4; //Reticula o grilla
+
+const azulejos = [];
+const NA = 16; //Numero de azulejos(imagenes)
+const rules = [//Reglas de bordes de cada azulejo
+  {//tile 0
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {//tile 1
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {//tile 2
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+  {//tile 3
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {//tile 4
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {//tile 5
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+  {//tile 6
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {//tile 7
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {//tile 8
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {//tile 9
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+  {//tile 10
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {//tile 11
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {//tile 12
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {//tile 13
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {//tile 14
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {//tile 15
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+];
+
+function preLoad(){
+  for (let i = 0; i < NA; i++) {
+    azulejos[i] = loadImage(`azulejos/tile${i}.png`);
+  }
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1080, 1080);
+  let opciInc = [];
+  for (let i = 0; i < azulejos.length; i++) {
+    opciInc.push(i);
+  }
+
+  for (let i = 0; i < RCTC * RCTC; i++) {
+    celd[i] = {
+      colapse: false,
+      opci: opciInc,
+    };
+  }
+  print (celd);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  
 }
